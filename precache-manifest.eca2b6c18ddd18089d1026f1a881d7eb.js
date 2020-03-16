65408449207551a45d357439ab06a1f5a6acf9fa
stadnikivan <stadnik11ivan@gmail.com>
2020-03-16 18:35:31 +0200

    Updates

---
+++ precache-manifest.eca2b6c18ddd18089d1026f1a881d7eb.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da1229cf1e0ab643c5edc31b7401976e",
+    "url": "/react7/index.html"
+  },
+  {
+    "revision": "8ee31ec9dbfefe8ec07b",
+    "url": "/react7/static/css/main.3cf1ad21.chunk.css"
+  },
+  {
+    "revision": "fcd840c115e4fb8233b6",
+    "url": "/react7/static/js/2.bc22e622.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/react7/static/js/2.bc22e622.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ee31ec9dbfefe8ec07b",
+    "url": "/react7/static/js/main.17265dc3.chunk.js"
+  },
+  {
+    "revision": "bc40ba6743e001bca8bb",
+    "url": "/react7/static/js/runtime-main.59f47a00.js"
+  },
+  {
+    "revision": "e3fbc2ed0a2575651b8f78a83c9a9859",
+    "url": "/react7/static/media/Chipolino.e3fbc2ed.jpg"
+  },
+  {
+    "revision": "ec77c85cb68e97784c20c4667cdc6ce1",
+    "url": "/react7/static/media/Hagrid.ec77c85c.jpg"
+  },
+  {
+    "revision": "5e0d4faabeadf9afd406c4ff3e3ada83",
+    "url": "/react7/static/media/Obama.5e0d4faa.jpg"
+  }
+]);
